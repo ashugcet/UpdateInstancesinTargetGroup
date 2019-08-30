b3df41bd9190747269f98f83f3526b53d18b2129
Ashutosh Kumar Dwivedi <ADwivedi@cpaglobal.com>
2019-08-30 18:06:53 +0530

    Add project files.

---
+++ UpdateInstancesInTargetGroups/UpdateInstancesInTargetGroups.py
@@ -0,0 +1,75 @@
+
+#!/usr/bin/python
+import boto3
+import sys
+import string
+session = boto3.session.Session(profile_name='alpha_gov_prod')
+elb_name = "arn:aws-us-gov:elasticloadbalancing:us-gov-west-1:367780343923:loadbalancer/app/green-tipms-cipp-pilot-mdc-lb-tf/adea379c2776bbe2"
+client = session.client('elbv2')
+
+response = client.describe_target_groups(
+    LoadBalancerArn=elb_name
+)
+
+for item in response['TargetGroups']:
+#  print (item['TargetGroupArn'])
+  if 'tfgreenpilot-dp' in  (item['TargetGroupArn']):
+    print(item['TargetGroupArn'])
+    print('---tfgreenpilot-dp---')
+#   response = client.register_targets(
+#      TargetGroupArn=(item['TargetGroupArn']),
+#      Targets=[
+#        {
+#            'Id': 'i-0a49fa95060c07f14',
+#        },
+#        {
+#            'Id': 'i-0a6ff7755a2a61f00',
+#        },
+#       ],
+#       )
+for item in response['TargetGroups']:
+  if 'tfgreenpilot-analytics' in  (item['TargetGroupArn']):
+      print(item['TargetGroupArn'])
+      print('---tfgreenpilot-analytics---')
+#      response = client.register_targets(
+#      TargetGroupArn=(item['TargetGroupArn']),
+#      Targets=[
+#          {
+#              'Id': 'i-0fe535a9ba3b2eefe',
+#          }
+#          ],
+#          )
+  elif 'tfgreenpilot-dvs' in  (item['TargetGroupArn']):
+     print(item['TargetGroupArn'])
+     print ("----------tfgreenpilot-dvs----------")
+#      response = client.register_targets(
+#       TargetGroupArn=(item['TargetGroupArn']),
+#      Targets=[
+#          {
+#              'Id': 'i-0f82dce8b9a1570ba',
+#          }
+#          ],
+#          )
+
+
+  elif 'tfgreenpilot-tipm-443' in  (item['TargetGroupArn']):
+     print(item['TargetGroupArn'])
+     print('----------tfgreenpilot-tipm-443--------')
+#      response = client.register_targets(
+#      TargetGroupArn=(item['TargetGroupArn']),
+#      Targets=[
+#          {
+#              'Id': 'i-0f82dce8b9a1570ba',
+#          }
+#          ],
+#          )
+  else:
+    print(item['TargetGroupArn'])
+    response = client.register_targets(
+      TargetGroupArn=(item['TargetGroupArn']),
+      Targets=[
+          {
+              'Id': 'i-09cace99a15db8fbc',
+          }
+          ],
+          )
